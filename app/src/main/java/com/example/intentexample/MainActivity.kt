4c0e85f4552779415caafb86479442c922ed04af
Willy <guillecg13@gmail.com>
2023-07-04 17:35:42 +0200

    Added: view binding and intent

--- app/src/main/java/com/example/intentexample/MainActivity.kt
+++ app/src/main/java/com/example/intentexample/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.intentexample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.intentexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnGreeting.setOnClickListener {
+            // Guardamos el nombre del input text
+            val name = binding.etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                // Se crea un objeto de tipo Intent que recibe el contexto (this) y la clase donde va.
+                val intent = Intent(this, ResultActivity::class.java)
+
+                // Enviamos al ResultActivity el contenido del input text
+                // Recibe 2 params: una clave y el valor que pasamos
+                intent.putExtra("EXTRA_NAME", name)
+
+                // Se lanza el intent
+                startActivity(intent)
+            }
+
+        }
     }
 }
